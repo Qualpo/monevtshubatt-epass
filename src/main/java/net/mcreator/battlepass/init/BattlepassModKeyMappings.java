7a6595c7a12bb767c832aaa972c65f3b33bc51cf
Qualpo <74394798+Qualpo@users.noreply.github.com>
2022-03-26 19:43:38 -0700

    08

--- src/main/java/net/mcreator/battlepass/init/BattlepassModKeyMappings.java
+++ src/main/java/net/mcreator/battlepass/init/BattlepassModKeyMappings.java
@@ -16,6 +16,9 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.KeyMapping;
 
+import net.mcreator.battlepass.network.OpenMenuMessage;
+import net.mcreator.battlepass.BattlepassMod;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
 public class BattlepassModKeyMappings {
 	public static final KeyMapping OPEN_MENU = new KeyMapping("key.battlepass.open_menu", GLFW.GLFW_KEY_B, "key.categories.ui");
@@ -30,6 +33,12 @@ public class BattlepassModKeyMappings {
 		@SubscribeEvent
 		public static void onKeyInput(InputEvent.KeyInputEvent event) {
 			if (Minecraft.getInstance().screen == null) {
+				if (event.getKey() == OPEN_MENU.getKey().getValue()) {
+					if (event.getAction() == GLFW.GLFW_PRESS) {
+						BattlepassMod.PACKET_HANDLER.sendToServer(new OpenMenuMessage(0, 0));
+						OpenMenuMessage.pressAction(Minecraft.getInstance().player, 0, 0);
+					}
+				}
 			}
 		}
 	}
