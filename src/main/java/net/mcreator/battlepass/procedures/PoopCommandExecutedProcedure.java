0de5b616c8777a54f0d7797d14c5818cb85c4e9d
Qualpo <74394798+Qualpo@users.noreply.github.com>
2022-03-26 22:07:00 -0700

    poop

---
+++ src/main/java/net/mcreator/battlepass/procedures/PoopCommandExecutedProcedure.java
@@ -0,0 +1,52 @@
+package net.mcreator.battlepass.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+public class PoopCommandExecutedProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z) {
+		if (world instanceof ServerLevel _level)
+			_level.getServer().getCommands()
+					.performCommand(
+							new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", new TextComponent(""),
+									_level.getServer(), null).withSuppressedOutput(),
+							"tellraw @p {\"text\":\"you pooped\",\"bold\":true,\"color\":\"yellow\"}");
+		new Object() {
+			private int ticks = 0;
+			private float waitTicks;
+			private LevelAccessor world;
+
+			public void start(LevelAccessor world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				MinecraftForge.EVENT_BUS.register(this);
+				this.world = world;
+			}
+
+			@SubscribeEvent
+			public void tick(TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					this.ticks += 1;
+					if (this.ticks >= this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				if (world instanceof Level _level && !_level.isClientSide())
+					_level.explode(null, x, y, z, 20, Explosion.BlockInteraction.DESTROY);
+				MinecraftForge.EVENT_BUS.unregister(this);
+			}
+		}.start(world, 60);
+	}
+}
